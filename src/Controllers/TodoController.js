bf412dcb2bea5a88e1c9ba9dcb0b66f9b0807df9
surujmiah6896 <surujmiah6896@gmail.com>
2025-07-02 00:44:09 +0600

    code uptimize create todo

--- src/Controllers/TodoController.js
+++ src/Controllers/TodoController.js
@@ -32,3 +32,22 @@ TodoController.filterTodos = async (req, res) => {
         res.status(500).json({ message: "Error fetching todos", error });
     }
 }
+
+// Create a new todo
+TodoController.createTodo = async (req, res) => {
+    const { title, description } = req.body;
+    const newTodo = new Todo({
+        title,
+        description,
+    });
+
+    try{
+        const savedTodo = await newTodo.save();
+        res.status(201).json({data: savedTodo, status: true, message: "Todo created successfully" });
+    }catch (error) {
+        console.error("Error creating todo:", error);
+        res.status(500).json({ message: "Error creating todo", error });
+    }
+  
+}
+
